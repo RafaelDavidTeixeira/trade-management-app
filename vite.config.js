bf9e46f61e770d7807027a383d59d6e5ed432f9c
Rafael David Teixeira <Rafael.david.teixeira@gailcom>
2025-05-31 11:33:08 -0300

    Corrige caminhos de recursos com base path no Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: O 'base' deve ser o nome do seu repositório no GitHub,
-  // SEMPRE com uma barra inicial e uma barra final.
-  base: '/trade-management-app/' 
+  base: '/trade-management-app/' // <--- ESTA LINHA É A CHAVE!
 })
