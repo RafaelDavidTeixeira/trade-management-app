f70277fbc26140d020cb7419292297b2cc34e07f
Rafael David Teixeira <Rafael.david.teixeira@gailcom>
2025-05-31 11:29:33 -0300

    Atualiza base path no vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-    },
-  },
-});
+  // IMPORTANTE: O 'base' deve ser o nome do seu repositório no GitHub,
+  // SEMPRE com uma barra inicial e uma barra final.
+  base: '/trade-management-app/' 
+})
